f7f4dbe003d3229c04e73ac772782a8307a60496
Kaushik Jain <107410207+jain2001@users.noreply.github.com>
2024-07-18 19:22:15 +0530

    Add files via upload

---
+++ ecom-proj/target/classes/data1.sql
@@ -0,0 +1,4 @@
+INSERT INTO product(name, desc, brand, price, category, release_date, available, quantity)
+VALUES
+    ('Nexon', 'sajsslh', 'Tata', 50, 'cars', '2024-01-08', true, 50),
+    ('Nexon', 'sajsslh', 'Tata', 50, 'cars', '2024-01-08', true, 50);
